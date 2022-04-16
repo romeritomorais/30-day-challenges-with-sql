abe7fbca6d6423ecd8fac7936fed77dd5a90b3ff
noarch <39017318+romeritomorais@users.noreply.github.com>
2022-04-16 02:49:29 -0300

    Add files via upload

---
+++ challenges/utils/install-miniconda.sh
@@ -0,0 +1,8 @@
+MINICONDA_INSTALLER_SCRIPT=Miniconda3-latest-Linux-x86_64.sh
+MINICONDA_PREFIX=/usr/local
+wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
+chmod +x $MINICONDA_INSTALLER_SCRIPT
+./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX
+
+
+conda install -c conda-forge ipython-sql
