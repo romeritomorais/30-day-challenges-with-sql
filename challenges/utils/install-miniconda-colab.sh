606e3d75bf821cdef4f9ffc117f128ca58999d77
noarch <39017318+romeritomorais@users.noreply.github.com>
2022-04-16 02:44:59 -0300

    Add files via upload

--- challenges/utils/install-miniconda-colab.sh
+++ challenges/utils/install-miniconda-colab.sh
@@ -1,4 +1,4 @@
-MINICONDA_INSTALLER_SCRIPT=Miniconda3-py39_4.11.0-Linux-x86_64.sh
+MINICONDA_INSTALLER_SCRIPT=Miniconda3-py39_4.12.0-Linux-x86_64.sh
 MINICONDA_PREFIX=/usr/local
 wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
 chmod +x $MINICONDA_INSTALLER_SCRIPT
